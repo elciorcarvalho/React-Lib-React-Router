66abfdd98cfb1405677af5ebedff3b5bb4ab75fd
elcior <elciorfscarvalho@gmail.com>
2022-07-06 12:45:59 -0300

    Inicio projeto Lib Raect Router

---
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { CgSearch } from "react-icons/cg/";
+
+import styles from './Buscador.module.scss'
+
+interface Props {
+    busca: string,
+    setBusca: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function Buscador({busca, setBusca}: Props) {
+    return (
+        <div className={styles.buscador}>
+            <input
+                value={busca}
+                onChange={(evento) => setBusca(evento.target.value)}
+                placeholder="Buscar"
+            />
+            <CgSearch
+                size={20}
+                color="#4C4D5E"
+            />
+        </div>
+    )
+}
